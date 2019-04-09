e130e10bba830636e111721669ec513810136a33
DotDotSpaceDotDot <47234073+DotDotSpaceDotDot@users.noreply.github.com>
2019-04-09 15:28:13 +1200

    Update settings.ini.php

--- settings.ini.php
+++ settings.ini.php
@@ -35,30 +35,6 @@ icon = "muximux-bullhorn"
 color = "#93c47d"
 enabled = "true"
 
-[Couchpotato]
-name = "CouchPotato"
-url = "http://locip:5050"
-scale = 1
-icon = "muximux-couchpotato"
-color = "#f85c22"
-enabled = "true"
-
-[Deluge]
-name = "Deluge"
-url = "http://locip:8112"
-scale = 1
-icon = "muximux-deluge"
-color = "#385171"
-enabled = "true"
-
-[Duplicati]
-name = "Duplicati"
-url = "http://locip:8200"
-scale = 1
-icon = "muximux-copy2"
-color = "#6fa8dc"
-enabled = "true"
-
 [Headphones]
 name = "Headphones"
 url = "http://locip:8282"
@@ -82,30 +58,6 @@ icon = "muximux-play2"
 color = "#674ea7"
 enabled = "true"
 
-[Lidarr]
-name = "Lidarr"
-url = "http://locip:8686"
-scale = 1
-icon = "muximux-music"
-color = "#93c47d"
-enabled = "true"
-
-[Minio]
-name = "Minio"
-url = "http://locip:9009"
-scale = 1
-icon = "muximux-stack"
-color = "#999999"
-enabled = "true"
-
-[NetData]
-name = "NetData"
-url = "http://locip:19999"
-scale = 1
-icon = "muximux-meter"
-color = "#cc0000"
-enabled = "true"
-
 [NZBGet]
 name = "NZBGet"
 url = "http://locip:6789"
@@ -120,14 +72,6 @@ url = "http://locip:3579"
 scale = 1
 icon = "muximux-plex"
 color = "#f9be03"
-enabled = "true"
-
-[Plex]
-name = "Plex"
-url = "http://locip:32400/web"
-scale = 1
-icon = "muximux-plex"
-color = "#df691a"
 enabled = "true"
 
 [Portainer]
@@ -146,26 +90,10 @@ icon = "muximux-sonarr"
 color = "#ffe599"
 enabled = "true"
 
-[SickChill]
-name = "SickChill"
-url = "http://locip:8081"
-scale = 1
-icon = "muximux-tv"
-color = "#6fa8dc"
-enabled = "true"
-
 [Sonarr]
 name = "Sonarr"
 url = "http://locip:8989"
 scale = 1
 icon = "muximux-sonarr"
 color = "#674ea7"
-enabled = "true"
-
-[Tautulli]
-name = "Tautulli"
-url = "http://locip:8181"
-scale = 1
-icon = "muximux-plexivity"
-color = "#cc7b19"
 enabled = "true"
